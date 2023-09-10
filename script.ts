326213bda17eebbcbd6b70c15061ebf3256105b9
Sibin George <57068771+Sibingeoge123@users.noreply.github.com>
2023-09-10 14:51:03 -0400

    add resume download

---
+++ script.ts
@@ -0,0 +1,18 @@
+// // JavaScript function to handle the button click event
+// document.getElementById("downloadButton").addEventListener("click", function () {
+//     // Create an anchor element
+//     var a = document.createElement("a");
+
+//     // Set the href and download attributes
+//     a.href = "path/to/your/file.pdf";
+//     a.download = "your-file-name.pdf"; // Optional, specify the download filename
+
+//     // Trigger a click event on the anchor element to initiate the download
+//     a.click();
+// });
+
+// function unlockButtons() {
+//     document.getElementById('unlock-new').onclick = () => {
+//         unlockPlus('new')
+//     }
+// }
